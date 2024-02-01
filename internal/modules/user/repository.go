35bea3077f292f1d9068e4bdbfc0c8918ee308e2
levniko <muhammet--aslan@hotmail.com>
2024-02-01 12:46:09 +0300

    Add user repository functions

--- internal/modules/user/repository.go
+++ internal/modules/user/repository.go
@@ -2,6 +2,7 @@ package users
 
 import (
 	"context"
+	"grpc-template/internal/database"
 	"grpc-template/internal/models"
 )
 
@@ -12,21 +13,31 @@ type IRepository interface {
 }
 
 type Repository struct {
-	//Connection database.IConnection
+	Connection database.IConnection
 }
 
-func GetUserRepository( /*c database.IConnection*/ ) *Repository {
-	return &Repository{ /*Connection: c*/ }
+func NewUserRepository(c database.IConnection) *Repository {
+	return &Repository{Connection: c}
 }
 
 func (r *Repository) Save(ctx context.Context, mdl *models.User) error {
-	return nil
+	return r.Connection.GetMaster().Create(mdl).Error
 }
 
 func (r *Repository) FindByEmail(ctx context.Context, email string) (*models.User, error) {
-	return nil, nil
+	var user models.User
+	err := r.Connection.GetSlave().Where("email = ?", email).Limit(1).Take(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
 
 func (r *Repository) FindByPhoneNumber(ctx context.Context, phoneNumber string) (*models.User, error) {
-	return nil, nil
+	var user models.User
+	err := r.Connection.GetSlave().Where("phone_number = ?", phoneNumber).Limit(1).Take(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
